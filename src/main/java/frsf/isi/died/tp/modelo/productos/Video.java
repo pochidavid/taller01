f71d079128039b6193aad3fa7518d8a0585b9efc
julisanchez <julianalejandrosanchez@hotmail.com>
2018-04-12 01:31:48 -0300

    Update Video.java

--- src/main/java/frsf/isi/died/tp/modelo/productos/Video.java
+++ src/main/java/frsf/isi/died/tp/modelo/productos/Video.java
@@ -2,25 +2,20 @@ package frsf.isi.died.tp.modelo.productos;
 
 public class Video extends MaterialCapacitacion {
 	
-	private static double costo_segundo;
+	private static double costo_segundo = 0.15;
 	private Integer  duracion;
 	
 	public Video(){
 		
 	}
 	public Video(Integer id, String titulo){
-		this.id=id;
-		this.titulo=titulo;
+		super(id,titulo);
 	}
 	public Video(Integer id, String titulo, Double costo){
-		this.id=id;
-		this.titulo=titulo;
-		costo_segundo=costo;
+		super(id,titulo,costo);
 	}
 	public Video(Integer id, String titulo, Double costo, Integer duracion){
-		this.id=id;
-		this.titulo=titulo;
-		costo_segundo=costo;
+		super(id,titulo,costo);
 		this.duracion=duracion;
 	}
 	@Override
@@ -32,13 +27,13 @@ public class Video extends MaterialCapacitacion {
 	@Override
 	public Boolean esLibro() {
 		// TODO Auto-generated method stub
-		return null;
+		return false;
 	}
 
 	@Override
 	public Boolean esVideo() {
 		// TODO Auto-generated method stub
-		return null;
+		return true;
 	}
 
 }
